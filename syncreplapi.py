45d9bf47a7244daf423e1f52d8b32b82995f5da8
Herwig bogaert <herwig.bogaert@viaa.be>
2020-09-06 23:15:17 +0200

    refactor

--- syncreplapi.py
+++ syncreplapi.py
@@ -1,10 +1,8 @@
 import re
 from socket import getfqdn
-from dateutil import parser
+from dateutil import parser as dateparser
 import connexion
 import ldap
-
-# Every ldapquery opensig
 
 # Ldap Reader object intended to be read from using the pyhton with statement
 class LdapReader:
@@ -26,37 +24,36 @@ class LdapServer:
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
     ldap.set_option(ldap.OPT_DEBUG_LEVEL, 0)
     FQDN = getfqdn()
-    RE_CSN = re.compile('(?P<timestamp>\d+).\d+(?P<timezone>[\w:+-])*#\d+#(?P<sid>\d+)#\d+')
 
     def __init__(self, uri):
         self.uri = uri
 
     # gets the csn of the base DSE for the given suffix and provider serverID
-    def get_csn(self, suffix, sid):
+    def get_csn(self, suffix, pid):
         with LdapReader(self.uri) as conn:
             result = conn.search_s(suffix, ldap.SCOPE_BASE, attrlist=['contextCSN'])
-            contextCSNs = [ x.decode() for x in result[0][1]['contextCSN'] ]
-            for contextCSN in contextCSNs:
-                m = self.RE_CSN.match(contextCSN)
-                # sid in contextCSN is hex
-                if int(m.group('sid'),16) != sid: continue
-                csn = parser.parse(m.group('timestamp') + m.group('timezone'))
-                break
-        return(csn)
+        g_contextCSN = ( self.parse_csn(x.decode()) for x in result[0][1]['contextCSN'] )
+        (sid, timestmap) = next(g_contextCSN)
+        while sid != pid: (sid,timestamp) = next(g_contextCSN)
+        return dateparser.parse(timestamp)
 
     def get_backends(self):
         with LdapReader(self.uri) as conn:
             result = conn.search_s('cn=config', ldap.SCOPE_SUBTREE,
                    filterstr='(objectClass=olcDatabaseConfig)', attrlist=['olcSuffix'])
-            backends = [ y['olcSuffix'][0].decode() for x,y in result if y.get('olcSuffix')]
-        return backends
+        return [ y['olcSuffix'][0].decode() for x,y in result if y.get('olcSuffix') ]
 
     @staticmethod
     def host2uri(uri):
-       return(re.match('ldap\w*://(\S*)',uri).group(1))
+       return re.match('ldap\w*://(\S*)', uri).group(1)
+
+    @staticmethod
+    def parse_csn(csn):
+        m = re.match('(?P<timestamp>\d+).\d+(?P<timezone>[\w:+-])*#\d+#(?P<sid>\d+)#\d+', csn)
+        # sid in contextCSN is hex
+        return (int(m.group('sid'),16), m.group('timestamp') + m.group('timezone'))
 
 class LdapProvider(LdapServer):
-    RE_SID = re.compile('(?P<sid>\d+)\s*(?P<uri>[\S]*)')
 
     def __init__(self):
         super().__init__('ldapi:///')
@@ -64,24 +61,23 @@ class LdapProvider(LdapServer):
         self.backends = self.get_backends()
         self.peers = self.get_peers()
 
-    def serverid_list(self):
+    def serverid_generator(self):
         with LdapReader(self.uri) as conn:
             result = conn.search_s('cn=config', ldap.SCOPE_BASE, attrlist=['olcServerID'])
-            serverIDs = [ self.RE_SID.match(x.decode()).groups() for x in result[0][1]['olcServerID'] ]
-        return serverIDs
+        return ( tuple(x.decode().split()) for x in result[0][1]['olcServerID'] )
 
     def get_id(self):
-        serverids = self.serverid_list()
-        id = [ sid for (sid,uri) in serverids if uri == '' or self.host2uri(uri) == self.FQDN ]
-        return int(id[0])
+        g_serverids = self.serverid_generator()
+        (sid,uri) = next(g_serverids)
+        while uri != '' and self.host2uri(uri) != self.FQDN: (sid, uri) = next(g_serverids)
+        return int(sid)
 
     def get_peers(self):
-        serverids = [ (sid, self.host2uri(uri)) for (sid, uri) in self.serverid_list() ]
+        serverids = [ (sid, self.host2uri(uri)) for (sid, uri) in self.serverid_generator() ]
         return set( [ host for (sid,host) in serverids if host != self.FQDN ] )
 
     def get_csn(self, suffix):
         return super().get_csn(suffix,self.id)
-
 
 def get_status(consumers):
     my_consumers = provider.peers.union(consumers)
@@ -109,4 +105,4 @@ provider = LdapProvider()
 app = connexion.App(__name__)
 app.add_api('swagger.yaml')
 app.run(port=8080)
-
+#
