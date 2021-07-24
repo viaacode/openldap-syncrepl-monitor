15541612ce0f1ac65cb3c8da61457b0a809bc545
Herwig bogaert <herwig.bogaert@viaa.be>
2021-07-24 20:37:19 +0200

    add comments + small refactor

--- syncreplinfo.py
+++ syncreplinfo.py
@@ -22,7 +22,7 @@ class LdapReader:
 class LdapServer:
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
     ldap.set_option(ldap.OPT_DEBUG_LEVEL, 0)
-    FQDN = getfqdn()
+    FQDN = getfqdn() # The fully quallified name of our host
 
     def __init__(self, uri):
         self.uri = uri
@@ -59,19 +59,29 @@ class LdapProvider(LdapServer):
         self.backends = self.get_backends()
         self.peers = self.get_peers()
 
+    # Returns the olcServerID attribute as a list of tuples (id, uri)
     def serverids(self):
         with LdapReader(self.uri) as conn:
             result = conn.search_s('cn=config', ldap.SCOPE_BASE, attrlist=['olcServerID'])
         return [ tuple(x.decode().split()) for x in result[0][1]['olcServerID'] ]
 
+    # Returns the id of the ldapserver on our host as an integer
+    # olcServerID can have a value with or without uri:
+    #   - with uri: the serverid of the instance is in the olcServerID
+    #     attribute with uri == fqdn of the our host
+    #   - without uri (len of the serverids tuple is 1): the current serverid
+    #     is the value of the olcServerID attribute.
     def get_id(self):
-        sid = [ s[0] for s in self.serverids() if len(s) == 1 or self.host2uri(s[1]) == self.FQDN ][0]
-        return int(sid)
+        sid = [ s[0] for s in self.serverids() if len(s) == 1 or self.host2uri(s[1]) == self.FQDN ]
+        return int(sid[0])
 
+    # If the uris in a multimaster setup are specified in the
+    # olcServerID attribute, then get_peers returns the peers of our host as a set
     def get_peers(self):
         peers = [ self.host2uri(uri) for (sid, uri) in self.serverids() ]
         return set(filter(lambda h: h != self.FQDN, peers))
 
+    # The provider's own csn
     def get_csn(self, suffix):
         return super().get_csn(suffix,self.id)
 
